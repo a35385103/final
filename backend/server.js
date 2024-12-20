9bfa626ab70c95d270b71b3a7f9183fd932c0c39
a35385103 <151005842+a35385103@users.noreply.github.com>
2024-12-20 23:51:45 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const path = require('path'); // 引入 path 模組來處理路徑
+const path = require('path');
 const app = express();
-const PORT = 3000;
 
-// 設定靜態檔案目錄，這樣 Express 會從 frontend 資料夾提供靜態檔案
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
-// 設定首頁路由
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
-// 啟動伺服器
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// 將 Express 應用包裝為無伺服器函數
+module.exports = (req, res) => {
+    app(req, res);
+};
